c331a27f4237e84e5dce1ce597a80677f5440883
clice lee <cliceleee@gmail.com>
2025-04-27 23:55:33 +0900

    Initial Commit

---
+++ JamPack/JamPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamPackApp.swift
+//  JamPack
+//
+//  Created by Clice Lee on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct JamPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
